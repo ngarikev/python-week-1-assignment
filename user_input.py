58f22bfed2b598b94ecac2ed0bc0b01e723f62b2
ngarikev <kevin.gary191@gmail.com>
2024-11-04 16:51:05 +0300

    done

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print("Happy birthday " + name + ". You have turned " + age + " years old. We will hold a birthday party at " + location )
+

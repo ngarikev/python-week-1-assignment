78164a1b9c54292b2815e74d206c96adb6ee7e12
ngarikev <kevin.gary191@gmail.com>
2024-11-04 16:54:48 +0300

    done

--- user_input.py
+++ user_input.py
@@ -1,6 +1,7 @@
+#input() functions that store user variables
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 location = input("Enter your location: ")
-
+# printing personalized message using user variables
 print("Happy birthday " + name + ". You have turned " + age + " years old. We will hold a birthday party at " + location )
 
